64b61168ce38e3b25d54939881178e85ab30caeb
Jacob Froman <jacob.h.froman@gmail.com>
2017-02-01 22:28:37 -0600

    added public folder

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 let app = express();
 
 app.set('port', 3000 || process.env.PORT);
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(app.get('port'), () => {
     console.log('Server listening on port', app.get('port'));
