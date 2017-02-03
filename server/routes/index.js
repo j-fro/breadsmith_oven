06c29048cf1b1d726c12e39fcb7059102c5b04e0
Jacob Froman <jacob.h.froman@gmail.com>
2017-02-03 09:43:50 -0600

    fixed server to startup and show index properly

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
-let router = express.Router;
+let router = express.Router();
 
-router.get('/', (req, res)=>{
-  let indexPath = path.join(__dirname,'../public/views/index.html');
-  res.sendFile(indexPath);
+router.get('/', (req, res) => {
+    let indexPath = path.join(__dirname, '../../public/views/index.html');
+    res.sendFile(indexPath);
 });
 
 module.exports = router;
