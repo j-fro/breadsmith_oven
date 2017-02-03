ef740d4ef8698aeff6a0cf8a6a97ea83595e0f9e
Karla Hugo <karlabrandon817@gmail.com>
2017-02-03 09:37:01 -0600

    index.js setup

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const pg = require('pg');
+const urlEncodedParser = bodyParser.urlencoded({extended: true});
+
 let app = express();
 
 app.set('port', 3000 || process.env.PORT);
@@ -10,3 +13,10 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.listen(app.get('port'), () => {
     console.log('Server listening on port', app.get('port'));
 });
+
+//require routers
+const indexRouter = require('.routes/index');
+
+
+//routers
+app.use('/', indexRouter);
