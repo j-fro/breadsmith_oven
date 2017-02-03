bb34c2cf151e6ebb296d6bcfe10549671d669f17
Jacob Froman <jacob.h.froman@gmail.com>
2017-02-03 11:27:49 -0600

    updated client functions to aggregrate client info

--- server/lib/clientlib.js
+++ server/lib/clientlib.js
@@ -3,10 +3,22 @@ const knex = require('../database/dbConfig');
 function getCustomerById(custId) {
     return new Promise((resolve, reject) => {
         knex
-            .select()
+            .select(
+                'customers.id',
+                'name',
+                'address',
+                'type',
+                'variety',
+                'price',
+                'regular',
+                'last_order_date',
+                'products.id as product_id'
+            )
             .from('customers')
-            .where('id', custId)
-            .then(customers => resolve(customers[0]))
+            .where('customers.id', custId)
+            .join('permitted_products', 'customers.id', 'customer_id')
+            .join('products', 'product_id', 'products.id')
+            .then(customers => resolve(aggregateCustomer(customers)))
             .catch(err => reject(err));
     });
 }
@@ -14,3 +26,35 @@ function getCustomerById(custId) {
 module.exports = {
     getCustomerById: getCustomerById
 };
+
+function aggregateCustomer(results) {
+    return results.reduce(
+        (obj, row) => {
+            obj.name = row.name;
+            obj.address = row.address;
+            obj.id = row.id;
+            obj.last_order_date = row.last_order_date;
+            if (typeof obj.products !== 'undefined') {
+                obj.products.push({
+                    id: row.product_id,
+                    type: row.type,
+                    variety: row.variety,
+                    price: row.price,
+                    regular: row.regular
+                });
+            } else {
+                obj.products = [
+                    {
+                        id: row.product_id,
+                        type: row.type,
+                        variety: row.variety,
+                        price: row.price,
+                        regular: row.regular
+                    }
+                ];
+            }
+            return obj;
+        },
+        {}
+    );
+}
