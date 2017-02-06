728bd2444c960140b429ab24a43b2031200cc4f9
Jacob Froman <jacob.h.froman@gmail.com>
2017-02-06 09:17:12 -0600

    added IF NOT EXISTS clause to ease migration

--- server/database/create_tables.sql
+++ server/database/create_tables.sql
@@ -1,32 +1,32 @@
-CREATE TABLE customers (
+CREATE TABLE IF NOT EXISTS customers (
     id SERIAL PRIMARY KEY,
     name VARCHAR NOT NULL,
     address VARCHAR,
     last_order_date DATE
 );
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS  users (
     id SERIAL PRIMARY KEY,
     email VARCHAR UNIQUE NOT NULL,
     role VARCHAR(10),
     customer_id INTEGER REFERENCES customers(id) ON DELETE SET NULL
 );
 
-CREATE TABLE products (
+CREATE TABLE IF NOT EXISTS  products (
     id SERIAL PRIMARY KEY,
     type VARCHAR NOT NULL,
     variety VARCHAR,
     price REAL NOT NULL
 );
 
-CREATE TABLE permitted_products (
+CREATE TABLE IF NOT EXISTS  permitted_products (
     id SERIAL PRIMARY KEY,
     customer_id INTEGER NOT NULL REFERENCES customers(id) ON DELETE CASCADE,
     product_id INTEGER NOT NULL REFERENCES products(id) ON DELETE CASCADE,
     regular BOOLEAN NOT NULL DEFAULT false
 );
 
-CREATE TABLE orders (
+CREATE TABLE IF NOT EXISTS  orders (
     id SERIAL PRIMARY KEY,
     total_qty INTEGER NOT NULL,
     total_cost REAL NOT NULL,
@@ -37,14 +37,14 @@ CREATE TABLE orders (
     customer_id INTEGER NOT NULL REFERENCES customers(id) ON DELETE CASCADE
 );
 
-CREATE TABLE order_items (
+CREATE TABLE IF NOT EXISTS  order_items (
     id SERIAL PRIMARY KEY,
     order_id INTEGER NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
     product_id INTEGER NOT NULL REFERENCES products(id) ON DELETE CASCADE,
     qty INTEGER NOT NULL
 );
 
-CREATE TABLE recurring_order_items (
+CREATE TABLE IF NOT EXISTS  recurring_order_items (
     id SERIAL PRIMARY KEY,
     customer_id INTEGER NOT NULL REFERENCES customers(id) ON DELETE CASCADE,
     product_id INTEGER NOT NULL REFERENCES products(id) ON DELETE CASCADE,
