92da94a35d71b552a4bdbcd7c8c8cff04c1e3f76
Karla Hugo <karlabrandon817@gmail.com>
2017-02-07 16:38:21 -0600

    config.js added

---
+++ public/scripts/config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDpavDLXpfMuNVyTWal1RaQTAoK3KbzXao",
+    authDomain: "breadsmith-50dc2.firebaseapp.com",
+    databaseURL: "https://breadsmith-50dc2.firebaseio.com",
+    storageBucket: "breadsmith-50dc2.appspot.com",
+    messagingSenderId: "510935484126"
+};
+firebase.initializeApp(config);
