cb71bdb9e66508fb2593b22e9269103c0b41dc96
Karla Hugo <karlabrandon817@gmail.com>
2017-02-07 15:10:24 -0600

    login alert

--- public/scripts/controllers/login.js
+++ public/scripts/controllers/login.js
@@ -14,7 +14,15 @@ myApp.controller('loginController', ['$scope', '$http', '$window', '$firebaseAut
 
         $scope.login = function() {
             console.log('login button clicked');
-            firebase.auth().signInWithPopup(provider);
+            firebase.auth().signInWithPopup(provider)
+            .then(function(firebaseUser){
+              console.log('logged in user:', firebaseUser);
+              alert('Logged in as' + ' ' + firebaseUser.user.displayName);
+            }).catch(function(error){
+              console.log('login failure:', error);
+              alert('Login Error!');
+              $window.location.href = '/';
+            });
         }; //end $scope.login
     }
 ]); //end loginController
