42f8e8acb000514f43206c011bc1b1fe05276eb3
Jacob Froman <jacob.h.froman@gmail.com>
2017-02-07 14:23:41 -0600

    allowed optional date

--- server/lib/reportLib.js
+++ server/lib/reportLib.js
@@ -3,29 +3,47 @@ const path = require('path');
 const json2csv = require('json2csv');
 const knex = require('../database/dbConfig');
 
-function getOrdersAndExport(filename) {
+function getOrdersAndExport(filename, beginDate, endDate) {
     knex
         .select()
         .from('orders')
         .join('order_items', 'orders.id', 'order_id')
         .join('products', 'product_id', 'products.id')
-        .then(result => exportOrderCsv(result, filename))
+        .then(result => exportCsv(result, filename))
         .catch(err => console.log(err));
 }
 
-function exportOrderCsv(orders, filename) {
-    console.log(typeof orders[0]);
-    let fields = Object.keys(orders[0]);
-    let csv = json2csv({data: orders, fields: fields});
-    fs.writeFile(
-        path.join(__dirname, '../../reports/', filename || 'order_report.csv'),
-        csv,
-        err => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('File Saved');
+function exportCsv(orders, filename) {
+    console.log(typeof orders);
+    if (orders[0]) {
+        let fields = Object.keys(orders[0]);
+        let csv = json2csv({data: orders, fields: fields});
+        fs.writeFile(
+            path.join(
+                __dirname,
+                '../../reports/',
+                filename || 'order_report.csv'
+            ),
+            csv,
+            err => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('File Saved');
+                }
             }
-        }
-    );
+        );
+    }
+}
+
+function getTallyAndExport(date) {
+    knex
+        .select('type')
+        .sum('qty')
+        .from('orders')
+        .join('order_items', 'orders.id', 'order_id')
+        .join('products', 'product_id', 'products.id')
+        .where('created', '>', date || new Date())
+        .groupBy('type')
+        .then(result => exportCsv(result, 'tally.csv'));
 }
