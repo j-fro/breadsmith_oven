0554accd28714fbfd8a3186734d5210ba53a9c56
Deforis Nash <defhnash@gmail.com>
2017-02-08 10:43:45 -0600

    edit and delete produts

--- public/scripts/controllers/products.js
+++ public/scripts/controllers/products.js
@@ -1,19 +1,48 @@
 myApp.controller("productRead", ["$scope", "$http", "$window", function($scope, $http, $window){
   var show = false;
-  $scope.show = show;
+  $scope.show = show;//hides the list before fetching(Will remove later)
     console.log("Products page");
     var array = ["one", "two", "three"];
-    $scope.list = array;
     $scope.getProducts = function(){
       $http({
         method:"GET",
         url:"/product"
       }).then(function(res){
-        console.log(res.data);
         array = res.data;
-        $scope.list = array;
-        show = true;
-        $scope.show = show;
-      });
+        $scope.list = array;//puts the products on the page
+        show = true;//prepares the list to be shown
+        $scope.show = show;//And shows it
+      });//end get call
+    };//end getProducts
+
+  $scope.editProduct = function(){
+    console.log("editing");
+    console.log(this);
+    var obj = {
+      id : this.product.id,
+      type: "aifferent"
     };
+    $http({
+      method:"PUT",
+      url:"/product",
+      data:obj
+    }).then(function(res){
+      console.log(res);
+    });//end http call
+    $scope.getProducts();
+  };//end editProduct
+  $scope.deleteProduct = function(){
+    console.log("deleting");
+    console.log(this);
+    var delObj = this.product.id;
+    $http({
+      method:"DELETE",
+      url : "/product/" + delObj ,
+      data : delObj
+    }).then(function(res){
+      console.log(res);
+    });//end http
+    $scope.getProducts();
+  };//end delete
+  $scope.getProducts();
 }]);
