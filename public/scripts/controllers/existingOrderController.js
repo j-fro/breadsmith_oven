1eaf5dc86c70738d0f18c4b0a916d11138ff3ecf
Jacob Froman <jacob.h.froman@gmail.com>
2017-02-07 19:50:28 -0600

    trying to get a date to show up..

--- public/scripts/controllers/existingOrderController.js
+++ public/scripts/controllers/existingOrderController.js
@@ -7,7 +7,10 @@ myApp.controller('ExistingOrderController', [
                 .get('/order/' + $scope.dateSelected)
                 .then(function(response) {
                     console.log(response);
-                    $scope.orders = response.data;
+                    $scope.orders = response.data.map(ord => {
+                        ord.created = new Date(ord.created);
+                        return ord;
+                    });
                 })
                 .catch(function(err) {
                     console.log(err);
