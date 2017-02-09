8cf1b49d02738ef3c778bb193779d2e4022221d0
Jacob Froman <jacob.h.froman@gmail.com>
2017-02-08 21:09:57 -0600

    implementing logout functionality and locking more views behind authentication

--- public/scripts/controllers/navController.js
+++ public/scripts/controllers/navController.js
@@ -1,9 +1,22 @@
 myApp.controller('NavController', [
     '$scope',
     '$window',
+    '$firebaseAuth',
     'AuthFactory',
-    function($scope, $window, AuthFactory) {
+    function($scope, $window, $firebaseAuth, AuthFactory) {
         console.log('NavController');
         $scope.auth = AuthFactory;
+        var auth = $firebaseAuth();
+
+        $scope.logout = function() {
+            auth
+                .$signOut()
+                .then(function() {
+                    $window.location.href = '#!/login';
+                })
+                .catch(function(err) {
+                    console.log(err);
+                });
+        };
     }
 ]);
