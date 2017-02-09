189ad7d017d1ddd14c9a556cacfcf8c8bca31c39
Jacob Froman <jacob.h.froman@gmail.com>
2017-02-08 20:27:52 -0600

    auth route now checks a user's token against the db for their role

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const auth = require('../util/auth');
+const knex = require('../database/dbConfig');
 
 let router = express.Router();
 
-router.get('/', (req, res) => {
-    let index = Math.floor(Math.random() * 2);
-    res.send(['admin', 'customer'][index]);
+router.get('/', auth.token, (req, res) => {
+    console.log(req.decodedToken.email);
+    knex
+        .select('role')
+        .from('users')
+        .where('email', req.decodedToken.email)
+        .then(role => res.send(role[0].role))
+        .catch(err => res.sendStatus(401));
+    // let index = Math.floor(Math.random() * 2);
+    // res.send(['admin', 'customer'][index]);
 });
 
 module.exports = router;
