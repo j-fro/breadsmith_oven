edd1efaea9964a5a10184604202fec698cc951c7
Karla Hugo <karlabrandon817@gmail.com>
2017-02-09 12:49:46 -0600

    get and put call workingin staff route

--- public/scripts/controllers/adminStaffController.js
+++ public/scripts/controllers/adminStaffController.js
@@ -11,41 +11,38 @@ myApp.controller('adminStaffController', ['$scope', '$http', '$window', 'AuthFac
             }, function errorCallback(error) {
                 console.log('error', error);
             });
-        };//end $scope.showStaff
-      $scope.showStaff();
+        }; //end $scope.showStaff
+        $scope.showStaff();
 
-    $scope.editStaff = function(){
-        console.log("editing", this);
-        var obj = {
-          id : this.id,
-          first_name : x.firstName,
-          last_name: $scope.xLastName,
-          email : $scope.xEmail,
-          role : $scope.xRole
-        };
-        console.log("obj is", obj);
-        $http({
-          method : "PUT",
-          url:"/staff",
-          data : obj
-        }).then(function(res){
-          console.log(res);
-          $scope.first_name = "";
-          $scope.last_name = "";
-          $scope.email="";
-          $scope.role="";
-          $scope.showStaff();
-        });
-        console.log(obj);
-      };//end editProduct
-
-            $scope.storeStaff = function(){
-              var x = this.staff;
-              console.log('this is x', x);
-              $scope.id = x.id;
-              $scope.xFirstName = x.first_name;
-              $scope.xLastName = x.last_name;
-              $scope.xEmail = x.email;
-              $scope.xRole = x.role;
+        $scope.editStaff = function() {
+            console.log("editing", this);
+            var objectToSend = {
+                id: this.id,
+                first_name: this.editFirst,
+                last_name: this.editLast,
+                email: this.editEmail,
+                role: this.editRole
             };
-}]);
+            console.log("obj is", objectToSend);
+            $http({
+                method: "PUT",
+                url: "/staff",
+                data: objectToSend
+            }).then(function(response) {
+                console.log(response);
+                location.reload();
+                $scope.showStaff();
+            });
+        }; //end editStaff
+
+        $scope.storeStaff = function() {
+            var x = this.staff;
+            console.log('this is x', x);
+            $scope.id = x.id;
+            $scope.xFirstName = x.first_name;
+            $scope.xLastName = x.last_name;
+            $scope.xEmail = x.email;
+            $scope.xRole = x.role;
+        }; //end $scope.storeStaff
+    }
+]);
