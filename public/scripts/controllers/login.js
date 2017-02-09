eec535912358d700942c7fcf61d56311cdfac6cb
Jacob Froman <jacob.h.froman@gmail.com>
2017-02-08 20:29:30 -0600

    added functionality to set factory tokens

--- public/scripts/controllers/login.js
+++ public/scripts/controllers/login.js
@@ -1,20 +1,35 @@
-myApp.controller('loginController', ['$scope', '$http', '$window', '$firebaseAuth',
-    function($scope, $http, $window, $firebaseAuth) {
+myApp.controller('LoginController', [
+    '$scope',
+    '$http',
+    '$firebaseAuth',
+    'AuthFactory',
+    function($scope, $http, $firebaseAuth, AuthFactory) {
         console.log('in loginController');
-
+        var auth = $firebaseAuth();
         var provider = new firebase.auth.GoogleAuthProvider();
 
         $scope.login = function() {
             console.log('login button clicked');
-            firebase.auth().signInWithPopup(provider)
-            .then(function(firebaseUser){
-              console.log('logged in user:', firebaseUser);
-              alert('Logged in as' + ' ' + firebaseUser.user.displayName);
-            }).catch(function(error){
-              console.log('login failure:', error);
-              alert('Login Error!');
-              $window.location.href = '/';
-            });
+            firebase
+                .auth()
+                .signInWithPopup(provider)
+                .then(function(firebaseUser) {
+                    console.log('logged in user:', firebaseUser);
+                    alert('Logged in as' + ' ' + firebaseUser.user.displayName);
+                })
+                .catch(function(error) {
+                    console.log('login failure:', error);
+                    alert('Login Error!');
+                });
         }; //end $scope.login
+
+        auth.$onAuthStateChanged(function(firebaseUser) {
+            if (firebaseUser) {
+                firebaseUser.getToken().then(function(idToken) {
+                    AuthFactory.setToken(idToken);
+                    AuthFactory.getRole();
+                });
+            }
+        });
     }
 ]); //end loginController
