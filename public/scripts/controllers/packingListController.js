4a28ac3941aa432cafc9f2fc42a9f55ba22f3255
Jacob Froman <jacob.h.froman@gmail.com>
2017-02-10 17:04:53 -0600

    more packing list formatting

--- public/scripts/controllers/packingListController.js
+++ public/scripts/controllers/packingListController.js
@@ -2,6 +2,7 @@ angular.module('packingApp', []).controller('PackingListController', [
     '$scope',
     '$window',
     function($scope, $window) {
+        $scope.today = new Date();
         $scope.orders = JSON.parse(localStorage.getItem('orders'));
         $window.print();
         setTimeout($window.close, 500);
