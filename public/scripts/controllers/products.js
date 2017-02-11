dfc5eb5ddf59617c123af23295c63440519c4516
Karla Hugo <karlabrandon817@gmail.com>
2017-02-11 13:59:29 -0600

    updated product page functionality

--- public/scripts/controllers/products.js
+++ public/scripts/controllers/products.js
@@ -1,87 +1,82 @@
-myApp.controller("productRead", ["$scope", "$http", "$window", function($scope, $http, $window){
-  var show = false;
-  $scope.show = show;//hides the list before fetching(Will remove later)
+myApp.controller("productRead", ["$scope", "$http", "$window", function($scope, $http, $window) {
+    var show = false;
+    $scope.show = show; //hides the list before fetching(Will remove later)
     console.log("Products page");
-    var array = ["one", "two", "three"];
-    $scope.getProducts = function(){
-      $http({
-        method:"GET",
-        url:"/product"
-      }).then(function(res){
-        console.log(res.data);
-        array = res.data;
-        $scope.list = array;//puts the products on the page
-        show = true;//prepares the list to be shown
-        $scope.show = show;//And shows it
-      });//end get call
-    };//end getProducts
-  $scope.editProduct = function(){
-    console.log("editing");
-    console.log("this is", this);
-    var obj = {
-      id : eObj.id.id,
-      type : this.editName,
-      variety : this.editVariety,
-      price : this.editPrice
-    };
-    console.log("obj is", obj);
-    $http({
-      method : "PUT",
-      url:"/product",
-      data : obj
-    }).then(function(res){
-      console.log(res);
-      $scope.editName = "";
-      $scope.editVariety="";
-      $scope.editPrice="";
-      $scope.getProducts();
-    });
-    console.log(obj);
-  };//end editProduct
-  $scope.deleteProduct = function(){
-    console.log("deleting");
-    console.log(this);
-    var delObj = this.product.id;
-    $http({
-      method:"DELETE",
-      url : "/product/" + delObj,
-      data : delObj
-    }).then(function(res){
-      console.log(res);
-      $scope.getProducts();
-    });//end http
 
-  };//end delete
-  $scope.getProducts();
-  var eObj={};
-  $scope.test = function(){
-    console.log(this);
-    console.log(eObj);
+    $scope.getProducts = function() {
+        $http({
+            method: "GET",
+            url: "/product"
+        }).then(function(res) {
+            console.log(res.data);
+            array = res.data;
+            $scope.list = res.data; //puts the products on the page
+            show = true; //prepares the list to be shown
+            $scope.show = show; //And shows it
+        }); //end get call
+    }; //end getProducts
 
-  };//end test
-  $scope.storeInfo = function(){
-    var x = this.product;
-    eObj.id = x;
-    $scope.pName = x.type;
-    $scope.pVariety = x.variety;
-    $scope.pPrice = x.price;
-  };//end storeInfo
-  $scope.newProduct = function(){
-    var nObj = {
-      type : $scope.newName,
-      variety : $scope.newVariety,
-      price : $scope.newPrice
+    $scope.storeInfo = function(product) {
+        $scope.productToEdit = JSON.parse(JSON.stringify(product));
+        console.log("productToEdit:", $scope.productToEdit);
+    };//end $scope.storeInfo
+
+    $scope.editProduct = function() {
+        console.log("editing:", $scope.productToEdit);
+        var obj = $scope.productToEdit;
+        $http({
+            method: "PUT",
+            url: "/product",
+            data: obj
+        }).then(function(res) {
+            console.log(res);
+            $scope.getProducts();
+        });
+    }; //end editProduct
+
+    $scope.deleteProduct = function() {
+        var delObj = $scope.productToEdit.id;
+        console.log("deleting:", delObj);
+        $http({
+            method: "DELETE",
+            url: "/product/" + delObj,
+            data: delObj
+        }).then(function(res) {
+            console.log(res);
+            $scope.getProducts();
+        }); //end http
+    }; //end delete
+
+    $scope.getProducts();
+    var eObj = {};
+    $scope.test = function() {
+        console.log(this);
+        console.log(eObj);
+
+    }; //end test
+
+
+    $scope.newProduct = function() {
+        var nObj = {
+            type: $scope.newName,
+            variety: $scope.newVariety,
+            price: $scope.newPrice
+        };
+        $http({
+            method: "POST",
+            url: "/product",
+            data: nObj
+        }).then(function(res) {
+            console.log("Post call response", res);
+            $scope.getProducts();
+            $scope.newName = "";
+            $scope.newVariety = "";
+            $scope.newPrice = "";
+        });
     };
-    $http({
-      method : "POST",
-      url : "/product",
-      data: nObj
-    }).then(function(res){
-      console.log("Post call response", res);
-      $scope.getProducts();
-      $scope.newName = "";
-      $scope.newVariety = "";
-      $scope.newPrice = "";
-    });
-  };
+    $scope.clearCreate=function(){
+      $scope.newName = '';
+      $scope.newVariety = '';
+      $scope.newPrice = '';
+    };//end $scope.clearCreate
 }]);
