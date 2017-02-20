8a834c90b183d1dd7c406e26c25fc88a61971046
Jacob Froman <jacob.h.froman@gmail.com>
2017-02-20 11:23:50 -0600

    added nav tab highlighting

--- public/scripts/controllers/navController.js
+++ public/scripts/controllers/navController.js
@@ -8,9 +8,19 @@ myApp.controller('NavController', [
         $scope.auth = AuthFactory;
         $scope.logout = AuthFactory.logOut;
         $scope.name = $scope.auth._State.name;
+        $scope.page = {};
+
+        $scope.selectNav = function(page) {
+            Object.keys($scope.page).forEach(function(key) {
+                $scope.page[key] = false;
+            });
+            $scope.page[page] = true;
+            console.log($scope.page);
+        };
 
         //-- navbar collapsing controls --//
         $scope.isNavCollapsed = true;
         $scope.isCollapsed = false;
         $scope.isCollapsedHorizontal = false;
-}]);
+    }
+]);
